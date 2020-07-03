dee471494bfce201d144f86d7c127500dc11e5d9
SapE <60630338+kimj9537@users.noreply.github.com>
2020-07-03 14:50:41 +1200

    asdf

---
+++ 09_unit_asker.py
@@ -0,0 +1,26 @@
+# Initialised lists
+length_unit = []
+expanses = []
+
+# Get inputs and add to item_cost list
+item = ""
+while length_unit.lower() != "xxx":
+    length_unit = []
+    unit = input(" Unit: ")
+
+    # If the user enters the exit code, break out of the loop
+    if unit.lower() == "xxx":
+      break
+
+    # Get the cost (replace with number checking function in due course)
+    length = float(input("Length: "))
+    lengths = ("${:.2f}".format(length))
+
+    # Add item name and cost to 'item' list
+    length_unit.append(item)
+    length_unit.append(length)
+
+    expanses.append(length_unit)
+
+for line_name in expanses:
+    print("{} : {}".format(length_unit[0], length_unit[1]))
